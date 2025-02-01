d7ece362251edb4413b27da1941f594da73253a5
Mairo022 <Mairo022@outlook.com>
2025-02-01 20:13:24 +0200

    Mov memory, and immediate to reg

---
+++ Encodings.cs
@@ -0,0 +1,40 @@
+﻿namespace CpuSimulation;
+
+public static class Encodings
+{
+    public static readonly string[] ModMem = 
+    [
+        "bx + si",
+        "bx + di",
+        "bp + si",
+        "bp + di",
+        "si",
+        "di",
+        "bp",
+        "bx",
+    ];
+
+    public static readonly string[] ModRegOnly8 =
+    [
+        "al",
+        "cl",
+        "dl",
+        "bl",
+        "ah",
+        "ch",
+        "dh",
+        "bh"
+    ];
+
+    public static readonly string[] ModRegOnly16 =
+    [
+        "ax",
+        "cx",
+        "dx",
+        "bx",
+        "sp",
+        "bp",
+        "si",
+        "di"
+    ];
+}
--- Program.cs
+++ Program.cs
@@ -1,49 +1,107 @@
-﻿// 1000100111011001
-//       0 dir
-//        1 width
-//         11 cx MOD
-//           011 bx REG
-//              001 R/M
+﻿using static CpuSimulation.Encodings;
 
 if (args.Length == 0) return;
 
 using var fileReadStream = new FileStream(args[0], FileMode.Open, FileAccess.Read);
-var buffer = new byte[2];
+var buffer = new byte[1];
 
 Console.WriteLine("bits 16\n");
+    
+var src = string.Empty;
+var dest = string.Empty;
 
-while (fileReadStream.Read(buffer, 0 , buffer.Length) > 0)
+while (fileReadStream.Read(buffer, 0 , 1) > 0)
 {
-    var binaryInput = BytesToBinary(buffer);
+    var binaryInput = BytesToBinaryStr(buffer);
 
-    if (binaryInput[..6] != "100010") continue;
-
-    var d = Convert.ToInt32(binaryInput[6].ToString(), 2);
-    var w = Convert.ToInt32(binaryInput[7].ToString(), 2);
-    var mod = Convert.ToInt32(binaryInput[8..10], 2);
-    var reg = Convert.ToInt32(binaryInput[10..13], 2);
-    var rm = Convert.ToInt32(binaryInput[13..], 2);
-
-    string src;
-    string dest;
-
-    if (w == 1)
+    if (binaryInput[..6] == "100010")
     {
-        src = ((Width16Encodings) reg).ToString();
-        dest = ((Width16Encodings) rm).ToString();
-    }
-    else
-    {
-        src = ((Width8Encodings) reg).ToString();
-        dest = ((Width8Encodings) rm).ToString();
+        var localBuffer = new byte[] { buffer[0], 0 };
+        fileReadStream.ReadExactly(localBuffer, 1, 1);
+        
+        var binaryStr = BytesToBinaryStr(localBuffer);
+        var d = Convert.ToInt16(binaryStr[6].ToString(), 2);
+        var w = Convert.ToInt16(binaryStr[7].ToString(), 2);
+        var mod = Convert.ToInt16(binaryStr[8..10], 2);
+        var reg = Convert.ToInt16(binaryStr[10..13], 2);
+        var rm = Convert.ToInt16(binaryStr[13..], 2);
+        
+        switch (mod)
+        {
+            case 0b00:
+                src = d == 1 
+                    ? AddressStr(ModMem[rm])
+                    : w == 1 ? ModRegOnly16[reg] : ModRegOnly8[reg];
+                dest = d == 1
+                    ? w == 1 ? ModRegOnly16[reg] : ModRegOnly8[reg]
+                    : AddressStr(ModMem[rm]);
+                
+                if (rm == 0b110)
+                {
+                    fileReadStream.ReadExactly(localBuffer, 0, 2);
+                    if (d == 1) src = $"[{BitConverter.ToInt16(localBuffer, 0).ToString()}]";
+                    if (d == 0) dest = $"[{BitConverter.ToInt16(localBuffer, 0).ToString()}]";
+                }
+                break;
+            case 0b01:
+                fileReadStream.ReadExactly(localBuffer, 0, 1);
+                var disp8 = (sbyte) localBuffer[0];
+                
+                src = d == 1
+                    ? AddressStr(ModMem[rm], disp8)
+                    : w == 1 ? ModRegOnly16[reg] : ModRegOnly8[reg];
+                dest = d == 1
+                    ? w == 1 ? ModRegOnly16[reg] : ModRegOnly8[reg]
+                    : AddressStr(ModMem[rm], disp8);
+                break;
+            case 0b10:
+                fileReadStream.ReadExactly(localBuffer, 0, 2);
+                var disp16 = BitConverter.ToInt16(localBuffer, 0);
+                
+                src = d == 1 
+                    ? AddressStr(ModMem[rm], disp16)
+                    : w == 1 ? ModRegOnly16[reg] : ModRegOnly8[reg];
+                dest = d == 1
+                    ? w == 1 ? ModRegOnly16[reg] : ModRegOnly8[reg]
+                    : AddressStr(ModMem[rm], disp16);
+                break;
+            case 0b11:
+                src = w == 1 ? ModRegOnly16[reg] : ModRegOnly8[reg];
+                dest = w == 1 ? ModRegOnly16[rm] : ModRegOnly8[rm];
+                break;
+        }
     }
 
+    if (binaryInput[..4] == "1011")
+    {
+        var localBuffer = new byte[2];
+        
+        var w = Convert.ToInt16(binaryInput[4].ToString(), 2);
+        var reg = Convert.ToInt16(binaryInput[5..], 2);
+
+        if (w == 0)
+        {
+            fileReadStream.ReadExactly(localBuffer, 0, 1);
+            
+            src = Convert.ToString(localBuffer[0]);
+            dest = ModRegOnly8[reg];
+        }
+        else
+        {
+            fileReadStream.ReadExactly(localBuffer, 0, 2);
+            var data = BitConverter.ToInt16(localBuffer, 0);
+
+            src = Convert.ToString(data);
+            dest = ModRegOnly16[reg];
+        }
+    }
+    
     Console.WriteLine($"mov {dest}, {src}");
 }
 
 return;
 
-static string BytesToBinary(byte[] bytes)
+static string BytesToBinaryStr(byte[] bytes)
 {
     var binaryString = string.Empty;
     
@@ -55,26 +113,7 @@ static string BytesToBinary(byte[] bytes)
     return binaryString;
 }
 
-enum Width8Encodings
+static string AddressStr(string addr, int disp = 0)
 {
-    al = 0,
-    cl = 1,
-    dl = 2,
-    bl = 3,
-    ah = 4,
-    ch = 5,
-    dh = 6,
-    bh = 7
-}
-
-enum Width16Encodings
-{
-    ax = 0,
-    cx = 1,
-    dx = 2,
-    bx = 3,
-    sp = 4,
-    bp = 5,
-    si = 6,
-    di = 7
+    return disp == 0 ? $"[{addr}]" : $"[{addr} + {disp}]";
 }
